299dfb985839e7bc2ac6832e6bb805125b69aab6
Sangeetha-sasikala <145675491+Sangeetha-sasikala@users.noreply.github.com>
2024-07-09 21:11:06 +0530

    Add files via upload

---
+++ HTML and CSS/script.js
@@ -0,0 +1,29 @@
+const display = document.getElementById('display');
+
+function appendCharacter(character) {
+    if (display.innerText === '0') {
+        display.innerText = character;
+    } else {
+        display.innerText += character;
+    }
+}
+
+function clearDisplay() {
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    if (display.innerText.length > 1) {
+        display.innerText = display.innerText.slice(0, -1);
+    } else {
+        display.innerText = '0';
+    }
+}
+
+function calculate() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
